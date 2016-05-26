e0dcc0c30ca4093d1188d5d67533c673daf2cb54
Daniel Pérez <dapanas@gmail.com>
2016-05-26 11:42:58 -0500

    Add empty entities for GasStation, Refuel and Vehicle.

---
+++ api/controllers/GasStationController.js
@@ -0,0 +1,11 @@
+/**
+ * GasStationController
+ *
+ * @description :: Server-side logic for managing Gasstations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/RefuelController.js
@@ -0,0 +1,11 @@
+/**
+ * RefuelController
+ *
+ * @description :: Server-side logic for managing Refuels
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
